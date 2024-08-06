00366238073ae8a501f0ef133ef5b6d6d51b3e0a
Samuel-Brito19 <sambriton7@gmail.com>
2024-08-06 11:30:56 -0300

    Making our server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,42 @@
-fn main() {
-    println!("Hello, world!");
+use actix_cors::Cors;
+use actix_web::{http::header, HttpServer};
+use sqlx::{postgres::PgPoolOptions, Pool, Postgres};
+use dotenv::dotenv;
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    if std::env::var_os("RUST_LOG").is_none() {
+        std::env::set_var("RUST_LOG", "actix_webi=info");
+    }
+    dotenv().ok();
+    env_logger::init();
+
+    let database_url: String = std::env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+    let pool: Pool<Postgres> = match PgPoolOptions::new()
+        .max_connections(10)
+        .connect(&database_url)
+        .await
+    {
+        Ok(pool) => {
+            println!("Connection successful");
+            pool
+        }
+        Err(err) => {
+            println!("Conection refused {:?}", err);
+            std::process::exit(1)
+        }
+    };
+
+    println!("Server starded successfully!")
+
+    HttpServer::new(move || {
+        let cors = Cors::default()
+            .allowed_origin("http:://locahost::3000")
+            .allowed_methods(vec!["GET", "POSTS", "DELETE", "PUT"])
+            .allowed_headers(vec![
+                header::CONTENT_TYPE,
+                header::AUTHORIZATION,
+                header::ACCEPT
+            ])
+            .supports_credentials();
+    })
 }
