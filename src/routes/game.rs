ed16b3922a74c0b5cd5943ee083e7bb400f2c42c
Samuel-Brito19 <sambriton7@gmail.com>
2024-08-07 19:20:01 -0300

    Get route

--- src/routes/game.rs
+++ src/routes/game.rs
@@ -1,4 +1,6 @@
+use actix_web::web;
 
+use crate::AppState;
 
 #[get("/games")]
 pub async fn get_games(data: web::Data<AppState>) -> impl Responder {
@@ -23,18 +25,21 @@ pub async fn get_games(data: web::Data<AppState>) -> impl Responder {
     HttpResponse::Ok().json(json_response)
 }
 
-#[post("/games/game")];
-async fn create_game(body: web::Json<CreateGameSchema>, data: web::Data<AppState>) -> impl Responder {
+#[post("/games/game")]
+async fn create_game(
+    body: web::Json<CreateGameSchema>,
+    data: web::Data<AppState>,
+) -> impl Responder {
     let query_result = sqlx::query_as!(
         "INSERT INTO games (name, description, price) VALUES ($1, $2, $3) RETURNING *",
         body.field_name.to_string(),
         body.address.to_string(),
-        body.day.to_string() 
+        body.day.to_string()
     )
     .fetch_one(&data.db)
     .await;
 
-    match query_result (
+    match query_result {
         Ok(game) => {
             let game_response = serde_json::json!({
                 "status": "success",
@@ -43,14 +48,48 @@ async fn create_game(body: web::Json<CreateGameSchema>, data: web::Data<AppState
                 })
             });
             return HttpResponse::Ok().json(game_response);
-
         }
         Err(e) => {
-            let message: &str = "Failed to create game";
-            return HttpResponse::BadRequest()
-                .json(serde_json::json!({"status": "error", "message": "Game with problem"}));
-        }
-    )
+            if e.to_string()
+                .contains("duplicate key value violates unique constraint")
+            {
+                return HttpResponse::BadRequest()
+                .json(serde_json::json!({"status": "fail","message": "Note with that title already exists"}));
+            }
 
-    HttpResponse::Ok().json(json_response)
+            return HttpResponse::InternalServerError()
+                .json(serde_json::json!({"status": "error","message": format!("{:?}", e)}));
+        }
+    }
+}
+
+#[get("/games/game/{id}")]
+async fn get_game_by_id(path: web::Path<uuid::Uuid>, data: web::Data<AppState>) -> impl Responder {
+    let game_id = path.into_inner();
+    let query_result = sqlx::query_as!(GameModel, "SELECT * FROM games WHERE id = $1", game_id)
+        .fetch_one(&data.db)
+        .await;
+    match query_result {
+        Ok(game) => {
+            let game_response = serde_json::json!({
+                "status": "success",
+                "data": serde_json::json!({
+                    "game": game
+                })
+            });
+            return HttpResponse::Ok().json(game_response);
+        }
+        Err(_) => {
+            let message = format!("Note with id {} not found", game_id);
+            return HttpResponse::NotFound()
+                .json(serde_json::json!({"status": "fail","message": message}));
+        }
+    }
+}
+
+#[put("/games/game/{id}")]
+async fn update_game_by(
+    path: web::Path<uuid::Uuid, data: web::Data<AppState>, body: web::Json<UpdateGamesSchema>>,
+) -> impl Responder {
+    let game_id = path.into_inner();
 }
