d651c34acca3f8f532bbd3adb7e95e27889599c4
Samuel-Brito19 <sambriton7@gmail.com>
2024-08-07 18:33:16 -0300

    Creating our routes

--- src/models/game.rs
+++ src/models/game.rs
@@ -4,7 +4,7 @@ pub struct GameModel {
     pub id: Uuid,
     pub name: String,
     pub address: String,
-    pub day: chrono::DateTime<chrono::Utc>,
+    pub day: String,
     pub created_at: Option<chrono::DateTime<chrono::Utc>>,
     pub updated_at: Option<chrono::DateTime<chrono::Utc>>,
 }
@@ -14,5 +14,12 @@ pub struct GameModel {
 pub struct CreateGameSchema {
     pub name: String,
     pub address: String,
-    pub date: chrono::DateTime<chrono::Utc>,
+    pub day: chrono::DateTime<chrono::Utc>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct UpdateGameSchema {
+    pub name: Option<String>,
+    pub address: Option<String>,
+    pub day: Option<chrono::DateTime<chrono::Utc>>,
 }
---
+++ src/routes/game.rs
@@ -0,0 +1,56 @@
+
+
+#[get("/games")]
+pub async fn get_games(data: web::Data<AppState>) -> impl Responder {
+    let query_result = sqlx::query_as!(GameModel, "SELECT * FROM games")
+        .fetch_all(&data.db)
+        .await;
+
+    if query_result.is_err() {
+        let message: &str = "Failed to fetch games";
+        return HttpResponse::InternalServerError()
+            .json(json!({"status": "error", "message": message}));
+    }
+
+    let games = query_result.unwrap();
+
+    let json_response = serde_json::json!({
+        "status": "success",
+        "no. games": games.len(),
+        "games": games
+    });
+
+    HttpResponse::Ok().json(json_response)
+}
+
+#[post("/games/game")];
+async fn create_game(body: web::Json<CreateGameSchema>, data: web::Data<AppState>) -> impl Responder {
+    let query_result = sqlx::query_as!(
+        "INSERT INTO games (name, description, price) VALUES ($1, $2, $3) RETURNING *",
+        body.field_name.to_string(),
+        body.address.to_string(),
+        body.day.to_string() 
+    )
+    .fetch_one(&data.db)
+    .await;
+
+    match query_result (
+        Ok(game) => {
+            let game_response = serde_json::json!({
+                "status": "success",
+                "data": serde_json::json!({
+                    "game": game
+                })
+            });
+            return HttpResponse::Ok().json(game_response);
+
+        }
+        Err(e) => {
+            let message: &str = "Failed to create game";
+            return HttpResponse::BadRequest()
+                .json(serde_json::json!({"status": "error", "message": "Game with problem"}));
+        }
+    )
+
+    HttpResponse::Ok().json(json_response)
+}
