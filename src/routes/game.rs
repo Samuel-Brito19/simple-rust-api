e06505f05af41f72767e3d907855ef23de1135bb
Samuel-Brito19 <sambriton7@gmail.com>
2024-08-08 08:36:40 -0300

    Updating route

--- src/routes/game.rs
+++ src/routes/game.rs
@@ -123,3 +123,53 @@ async fn create_note_handler(
         }
     }
 }
+
+#[patch("/notes/{id}")]
+async fn edit_note_handler(
+    path: web::Path<uuid::Uuid>,
+    body: web::Json<UpdateNoteSchema>,
+    data: web::Data<AppState>,
+) -> impl Responder {
+    let note_id = path.into_inner();
+    let query_result = sqlx::query_as!(NoteModel, "SELECT * FROM notes WHERE id = $1", note_id)
+        .fetch_one(&data.db)
+        .await;
+
+    if query_result.is_err() {
+        let message = format!("Note with ID: {} not found", note_id);
+        return HttpResponse::NotFound()
+            .json(serde_json::json!({"status": "fail","message": message}));
+    }
+
+    let now = Utc::now();
+    let note = query_result.unwrap();
+
+    let query_result = sqlx::query_as!(
+        NoteModel,
+        "UPDATE notes SET title = $1, content = $2, category = $3, published = $4, updated_at = $5 WHERE id = $6 RETURNING *",
+        body.title.to_owned().unwrap_or(note.title),
+        body.content.to_owned().unwrap_or(note.content),
+        body.category.to_owned().unwrap_or(note.category.unwrap()),
+        body.published.unwrap_or(note.published.unwrap()),
+        now,
+        note_id
+    )
+    .fetch_one(&data.db)
+    .await
+    ;
+
+    match query_result {
+        Ok(note) => {
+            let note_response = serde_json::json!({"status": "success","data": serde_json::json!({
+                "note": note
+            })});
+
+            return HttpResponse::Ok().json(note_response);
+        }
+        Err(err) => {
+            let message = format!("Error: {:?}", err);
+            return HttpResponse::InternalServerError()
+                .json(serde_json::json!({"status": "error","message": message}));
+        }
+    }
+}
